c222ddb4888ca9cecd7e7649722ec7f7a52a3f01
KodaiMeboso-Java <muxiguangda@gmail.com>
2023-01-21 15:22:57 +0900

    task5 jumphero->jumpHero

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<JumpHero> jumpheros = new ArrayList<>();
-        jumpheros.add(new JumpHero("ONE PIECE", "ルフィ", 1997));
-        jumpheros.add(new JumpHero("ドラゴンボール", "孫悟空", 1984));
-        jumpheros.add(new JumpHero("ドラゴンクエストダイの大冒険", "勇者ダイ", 1989));
-        jumpheros.add(new JumpHero("HUNTER☓HUNTER", "ゴン＝フリークス", 1998));
-        jumpheros.add(new JumpHero("NARUTO", "うずまきナルト", 1999));
-        jumpheros.add(new JumpHero("家庭教師ヒットマンリボーン", "沢田綱吉", 2004));
-        jumpheros.add(new JumpHero("ワールドトリガー", "空閑遊馬", 2013));
+        List<JumpHero> jumpHeros = new ArrayList<>();
+        jumpHeros.add(new JumpHero("ONE PIECE", "ルフィ", 1997));
+        jumpHeros.add(new JumpHero("ドラゴンボール", "孫悟空", 1984));
+        jumpHeros.add(new JumpHero("ドラゴンクエストダイの大冒険", "勇者ダイ", 1989));
+        jumpHeros.add(new JumpHero("HUNTER☓HUNTER", "ゴン＝フリークス", 1998));
+        jumpHeros.add(new JumpHero("NARUTO", "うずまきナルト", 1999));
+        jumpHeros.add(new JumpHero("家庭教師ヒットマンリボーン", "沢田綱吉", 2004));
+        jumpHeros.add(new JumpHero("ワールドトリガー", "空閑遊馬", 2013));
 
-        jumpheros.stream().filter(j -> j.getYear() <= 2000).forEach(System.out::println);
+        jumpHeros.stream().filter(j -> j.getYear() <= 2000).forEach(System.out::println);
     }
 }
